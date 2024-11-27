e6610d7b8caf9ed83025840868436f0060e0b0fb
tparnell <t.parnell96@icloud.com>
2024-11-27 11:10:42 -0600

    removed extra file

--- src/handlers/clear.rs
+++
@@ -1,9 +0,0 @@
-// src/handlers/clear.rs
-
-use crossterm::{execute, terminal::{Clear, ClearType}};
-use std::io::{stdout, Write};
-
-pub fn clear_screen() {
-    let mut stdout = stdout();
-    execute!(stdout, Clear(ClearType::All)).expect("Failed to clear screen");
-}
